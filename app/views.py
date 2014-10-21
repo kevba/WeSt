00ee13ed58a227478f56715e90deb257ddecc165
Kevin Batema <kevinbatema@gmail.com>
2014-10-21 15:33:35 +0100

    added a htmlpage for the map

--- app/views.py
+++ app/views.py
@@ -6,3 +6,8 @@ from app import west
 def show_home():
 	"""Shows the home page."""
 	return render_template("index.html")
+
+@west.route("/")
+def show_home():
+	"""Shows the map page."""
+	return render_template("map.html")
